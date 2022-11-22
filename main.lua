37fd5bc3e3738a4665c3a9269ae7647ec6817965
Dario Cangialosi Arkenidar (DCA) <dario.cangialosi@gmail.com>
2022-11-22 12:02:56 +0100

    step 2: 3D rotations (generalized), multi-polygon draw first steps, colors definitions for polygon and polygon vertex. 2 colored opposite quads (2 planar polygons). opposite and colored and with back-face culling so only 1 at a time is shown.

--- main.lua
+++ main.lua
@@ -5,57 +5,126 @@ end
 
 dofile('polygon.lua')
 
-polygon={{x=50,y=50}, {x=50+100,y=50}, {x=40,y=10} }
+polygon={
+  
+  {x=0,y=0,z=0},
+  {x=50,y=0,z=0},
+  {x=50,y=50,z=0},
+  {x=0,y=50,z=0},
+  
+  color={0,1,1},
+}
+polygon_back_face={
+  polygon[4],
+  polygon[3],
+  polygon[2],
+  polygon[1],
+  color={1,0,0},
+}
 
-degrees = 0.0
 
-function love.update(dt)
+polygons_original={polygon, polygon_back_face}
 
-  degrees = (degrees + dt*1000/60 ) % 360
-
+function polygon_transform(polygon)
+  
   local radiants = (degrees/360)*(math.pi*2)
   
-  function point_translate(point, x,y)
-    return {
-      x = point.x+x ,
-      y = point.y+y }
+  for i,point in ipairs(polygon) do
+    point.color = polygon.color
   end
   
+  function point_translate(point, x,y,z)
+    return {
+      x = point.x + x ,
+      y = point.y + y ,
+      z = point.z + z ,
+      color = point.color }
+  end
+  
+  --[[
   function point_rotate(point, radiants)
     return {
       x = math.cos(radiants)*point.x - math.sin(radiants)*point.y ,
       y = math.cos(radiants)*point.y + math.sin(radiants)*point.x }
+  end --]]
+  
+  function point_rotate_axes(axes, point_in, radiants)
+    local point_out = {x=point_in.x, y=point_in.y, z=point_in.z, color=point_in.color}
+    local one, two = string.sub(axes, 1,1), string.sub(axes, 2,2)
+    point_out[one] = math.cos(radiants)*point_in[one] - math.sin(radiants)*point_in[two]
+    point_out[two] = math.cos(radiants)*point_in[two] + math.sin(radiants)*point_in[one]
+    return point_out
   end
   
-  polygon_origin={}
+  function point_rotate_z(point, radiants)
+    return point_rotate_axes('xy', point, radiants)
+  end
+  
+  function point_rotate_y(point, radiants)
+    return point_rotate_axes('xz', point, radiants)
+  end
+  
+  local polygon_origin={}
   for i,point in ipairs(polygon) do
-    table.insert( polygon_origin, point_translate(point, -50,-50) )
+    table.insert( polygon_origin, point_translate(point, -50,-50,0) )
   end
   
-  polygon_rotated={}
+  local polygon_rotated1={}
   for i,point in ipairs(polygon_origin) do
-    table.insert( polygon_rotated, point_rotate(point, radiants) )
+    table.insert( polygon_rotated1, point_rotate_z(point, radiants) )
   end
   
-  polygon_rotated_translated={}
-  for i,point in ipairs(polygon_rotated) do
-    table.insert( polygon_rotated_translated, point_translate(point, 150,150) )
+  local polygon_rotated2={}
+  for i,point in ipairs(polygon_rotated1) do
+    table.insert( polygon_rotated2, point_rotate_y(point, radiants) )
   end
+  
+  local polygon_rotated_translated={}
+  for i,point in ipairs(polygon_rotated2) do
+    table.insert( polygon_rotated_translated, point_translate(point, 150,150,0) )
+  end
+  
+  local polygon_transformed = polygon_rotated_translated
+  
+  return polygon_transformed
+end
+
+degrees = 0.0
+
+function love.update(dt)
+  degrees = (degrees + dt*1000/60 ) % 360
+  
+  function polygons_transform(polygons)
+    local polygons_transformed = {}
+    for i,polygon in ipairs(polygons) do
+      table.insert(polygons_transformed, polygon_transform(polygon) )
+    end
+    return polygons_transformed
+  end
+  
+  polygons_transformed = polygons_transform(polygons_original)
 end
 
 function love.draw()
   
   for px=0,500 do
     for py=0,500 do
-      
-      local check = false
-      check = in_convex_polygon(px, py, polygon_rotated_translated)
-      
-      if check then
-        -- draw pixel
-        love.graphics.rectangle("fill", px,py, 1,1)
+      for i,polygon_iterated in ipairs(polygons_transformed) do
+        
+        local check = false
+        check = in_convex_polygon(px, py, polygon_iterated)
+        
+        if check then
+          
+          -- color pixel (vertex color, not polygon color!)
+          local rgb = polygon_iterated[1].color or {0,1,0} -- default if missing
+          love.graphics.setColor( rgb[1], rgb[2], rgb[3], 1 )
+          
+          -- draw pixel
+          love.graphics.rectangle("fill", px,py, 1,1)
+        end
+        
       end
-      
     end
   end
   
