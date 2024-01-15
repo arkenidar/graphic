b7c90efc2fe3969e68dadc6666d073d6493a2060
Dario Cangialosi <dario.cangialosi@gmail.com>
2024-01-15 15:47:16 +0100

    added require("lldebugger").start() for debug with tomblind.local-lua-debugger-vscode

--- main.lua
+++ main.lua
@@ -1,9 +1,10 @@
-
 -- main.lua is for Love2D (https://love2d.org)
+
+require("lldebugger").start()
 
 require('common-preceding')
 
-if love==nil then
+if love == nil then
   -- not using Love2D
   print("USAGE ERROR: run it with Love2D (get it at love2d.org)")
   os.exit(1)
@@ -12,10 +13,9 @@ end
 function love.load()
   -- from: http://notebook.kulchenko.com/zerobrane/love2d-debugging
   if arg[#arg] == "-debug" then require("mobdebug").start() end
-  
+
   love.window.setTitle("")
-  love.window.setMode(800,600)
-  
+  love.window.setMode(800, 600)
 end
 
 require('common')
@@ -29,11 +29,11 @@ function love.update(dt)
 end
 
 function draw_pixel(rgb, xy)
-  love.graphics.setColor( rgb[1], rgb[2], rgb[3], 1 )
+  love.graphics.setColor(rgb[1], rgb[2], rgb[3], 1)
 
   -- draw pixel
   local px, py = xy[1], xy[2]
-  love.graphics.rectangle("fill", px,py, 1,1)
+  love.graphics.rectangle("fill", px, py, 1, 1)
 end
 
 function love.draw()
