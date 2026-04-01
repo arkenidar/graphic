f76a9693b1f298ea75140e70d207c5dd6c50e73c
Dario Cangialosi @Arkenidar <dario.cangialosi@gmail.com>
2026-04-01 22:11:47 +0200

    refactor: uncomment SDL_image loading for improved library handling

--- app.lua
+++ app.lua
@@ -25,7 +25,7 @@ https://gist.github.com/creationix/1213280/a97d7051decb2f1d3e8844186bbff49b64427
 ffi.cdef(io.open("ffi_defs.h", "r"):read("*a"))
 
 --- local SDL = ffi.load("SDL2")
-local SDL_image  ----= ffi.load('SDL2_image')
+--- local SDL_image = ffi.load("SDL2_image")
 
 local function ffi_load_any(...)
     local last_error
@@ -41,6 +41,7 @@ local function ffi_load_any(...)
 end
 
 local SDL = ffi_load_any("SDL2", "libSDL2-2.0.so.0", "libSDL2.so")
+local SDL_image = ffi_load_any("SDL2_image", "libSDL2_image-2.0.so.0", "libSDL2_image.so")
 
 _G =
     setmetatable(
