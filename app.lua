63ac1a064dc90e6a2065c3ba8bb2c049b4b79061
Dario Cangialosi @Arkenidar <dario.cangialosi@gmail.com>
2026-04-01 21:40:11 +0200

    refactor: improve code formatting and indentation for better readability

--- app.lua
+++ app.lua
@@ -3,10 +3,10 @@
 
 ---require('common-preceding')
 
-if type(jit) ~= 'table' then
-  -- not using LuaJIT
-  print("USAGE ERROR: run it with LuaJIT (get it at luajit.org)")
-  os.exit(1)
+if type(jit) ~= "table" then
+    -- not using LuaJIT
+    print("USAGE ERROR: run it with LuaJIT (get it at luajit.org)")
+    os.exit(1)
 end
 
 -- from: http://notebook.kulchenko.com/zerobrane/love2d-debugging
@@ -22,50 +22,57 @@ https://gist.github.com/creationix/1213280/a97d7051decb2f1d3e8844186bbff49b64427
 --     echo '#include <SDL.h>' > stub.c
 --     gcc -I /usr/include/SDL -E stub.c | grep -v '^#' > ffi_SDL.h
 --]]
-ffi.cdef(io.open('ffi_defs.h', 'r'):read('*a'))
+ffi.cdef(io.open("ffi_defs.h", "r"):read("*a"))
 
-local SDL = ffi.load('SDL2')
-local SDL_image ----= ffi.load('SDL2_image')
+local SDL = ffi.load("SDL2")
+local SDL_image  ----= ffi.load('SDL2_image')
 
-_G = setmetatable(_G, {
-  __index = function(self, index) -- index function CASE
-    if "SDL" == string.sub(index, 1, 3) then
-      return SDL[index]
-    end
-    if "IMG" == string.sub(index, 1, 3) then
-      return SDL_image[index]
-    end
-  end
-})
+_G =
+    setmetatable(
+    _G,
+    {
+        __index = function(self, index) -- index function CASE
+            if "SDL" == string.sub(index, 1, 3) then
+                return SDL[index]
+            end
+            if "IMG" == string.sub(index, 1, 3) then
+                return SDL_image[index]
+            end
+        end
+    }
+)
 
 SDL_Init(0)
 local window = SDL_CreateWindow("[arkenidar/graphic] luajit app.lua", 50, 50, 300, 300, 0)
 local window_surface = SDL_GetWindowSurface(window)
 
 function rect_from_xywh(xywh)
-  if xywh == nil then return nil end
-  local rect = ffi.new('SDL_Rect')
-  rect.x = xywh[1]
-  rect.y = xywh[2]
-  rect.w = xywh[3] or 1
-  rect.h = xywh[4] or 1
-  return rect
+    if xywh == nil then
+        return nil
+    end
+    local rect = ffi.new("SDL_Rect")
+    rect.x = xywh[1]
+    rect.y = xywh[2]
+    rect.w = xywh[3] or 1
+    rect.h = xywh[4] or 1
+    return rect
 end
 
 function surface_draw_rect(rgb, xywh)
-  SDL_FillRect(window_surface, rect_from_xywh(xywh), SDL_MapRGB(window_surface.format, rgb[1], rgb[2], rgb[3]))
+    SDL_FillRect(window_surface, rect_from_xywh(xywh), SDL_MapRGB(window_surface.format, rgb[1], rgb[2], rgb[3]))
 end
 
 --draw_pixel = surface_draw_rect
 function draw_pixel(rgb, xy)
-  local rgb255 = {
-    rgb[1] * 255,
-    rgb[2] * 255,
-    rgb[3] * 255 }
-  surface_draw_rect(rgb255, xy)
+    local rgb255 = {
+        rgb[1] * 255,
+        rgb[2] * 255,
+        rgb[3] * 255
+    }
+    surface_draw_rect(rgb255, xy)
 end
 
-require('common')
+require("common")
 
 --[[
 function draw()
@@ -94,34 +101,31 @@ function draw()
 end
 
 --]]
-
 local time_ticks = SDL_GetTicks()
 local event = ffi.new("SDL_Event")
 local looping = true
 while looping do
-  while SDL_PollEvent(event) ~= 0 do
-    if event.type == SDL_QUIT or
-        (event.type == SDL_KEYDOWN and event.key.keysym.sym == SDLK_ESCAPE)
-    then
-      looping = false
+    while SDL_PollEvent(event) ~= 0 do
+        if event.type == SDL_QUIT or (event.type == SDL_KEYDOWN and event.key.keysym.sym == SDLK_ESCAPE) then
+            looping = false
+        end
     end
-  end
 
-  local dt -- elapsed time in fractions of seconds
-  delta_ticks = SDL_GetTicks() - time_ticks
-  time_ticks = SDL_GetTicks()
-  dt = delta_ticks / 1000 -- milliseconds to seconds
+    local dt  -- elapsed time in fractions of seconds
+    delta_ticks = SDL_GetTicks() - time_ticks
+    time_ticks = SDL_GetTicks()
+    dt = delta_ticks / 1000 -- milliseconds to seconds
 
-  update(dt)
+    update(dt)
 
-  --SDL_FillRect(window_surface,nil,0)
-  surface_draw_rect({ 0, 0, 0 })
+    --SDL_FillRect(window_surface,nil,0)
+    surface_draw_rect({0, 0, 0})
 
-  ---surface_draw_rect({0,255,0}, {50,50}) -- test pixel draw
+    ---surface_draw_rect({0,255,0}, {50,50}) -- test pixel draw
 
-  draw()
+    draw()
 
-  SDL_UpdateWindowSurface(window)
+    SDL_UpdateWindowSurface(window)
 end
 
 SDL_DestroyWindow(window)
